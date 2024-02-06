363a09df55616039a604a1b81ce8f9ad65d3269b
yosnak13 <ynakai@circlace.com>
2024-02-06 14:23:34 +0900

    for and while

---
+++ archive/step4/7_for_while/main.js
@@ -0,0 +1,20 @@
+// for文、繰り返し処理
+let result = 0;
+
+for (let i = 0; i <= 10; i++) {
+  result += i;
+}
+
+console.log(result);
+
+
+// while文、あまり使わない
+let result2 = 0;
+let k = 1;
+
+while (k <= 10) {
+  result2 += k;
+  k++
+}
+
+console.log(result2);
